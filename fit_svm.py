79bc8a0376b90ffd81c6c32973435155c35d2e5a
Kristin <kalinn@ncsu.edu>
2015-07-28 17:25:55 -0400

    Add class weights

--- fit_svm.py
+++ fit_svm.py
@@ -5,13 +5,17 @@ def fit_svm(x, y, ts, C):
 	#convert to numpy arrays
 	nX = np.array(x,ndmin=2)
 	ntsX = np.array(ts,ndmin=2)
-	nY = np.array(y,ndmin=1)/1.0
+	nY = np.array(y,ndmin=1)
+	dY = nY/1.0
+	pos_wt = dY.size/sum(dY)
+	neg_wt = 1/(1 - sum(dY)/dY.size)
 	#for tuning
+	wts = {0: neg_wt, 1: pos_wt}
 	paramGrid = [
 	{'C': C, 'kernel': ['linear']}
 	]
 	#train
-	trnMdl = svm.SVC()
+	trnMdl = svm.SVC(class_weight=wts)
 	trnMdl_grid = grid_search.GridSearchCV(trnMdl, paramGrid)
 	trnMdl_grid.fit(X=nX, y=nY)
 	trnMdl_best = trnMdl_grid.best_estimator_
